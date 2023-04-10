729c9be82f3421a3161acfa11de54a32622bbb6c
reiya0104 <reiya0104@ezweb.ne.jp>
2023-04-11 03:12:47 +0900

    feat: 残り時間を可視化
    DountTimer を実装

--- lib/widgets/dounut_chart/dount_timer.dart
+++ lib/widgets/dounut_chart/dount_timer.dart
@@ -11,13 +11,23 @@ class DountTimer extends StatelessWidget {
       {Key? key, required this.remainingSeconds, required this.timerMode})
       : super(key: key);
 
+  double getDountRate() {
+    int maxSeconds = initialSeconds[timerMode]!;
+    double dountRate = (remainingSeconds.toDouble()) / (maxSeconds.toDouble());
+
+    return dountRate;
+  }
+
   @override
   Widget build(BuildContext context) {
+    Color dountColor = dountColorMap[timerMode]!;
+    double dountRate = getDountRate();
+
     return DonutChart(
       chartRadius: 150,
       chartDataList: [
-        ChartData(color: Colors.blue, value: 1.0),
-        ChartData(color: Colors.grey, value: 0.0),
+        ChartData(color: dountColor, value: dountRate),
+        ChartData(color: Colors.grey, value: 1.0 - dountRate),
       ],
       chartStrokeWidth: 10,
     );
