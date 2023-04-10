ecd1069a053bea485d782184cdaeade844aef5a2
reiya0104 <reiya0104@ezweb.ne.jp>
2023-04-10 23:18:09 +0900

    feat: TimerMode に関するテストコードを追加

--- test/widgets/timer_dial_test.dart
+++ test/widgets/timer_dial_test.dart
@@ -1,38 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pomodoro/models/timer_mode.dart';
 import 'package:flutter_pomodoro/widgets/timer_dial.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('TimerDial', () {
-    testWidgets('should display 25:00 initially', (WidgetTester tester) async {
+    testWidgets('should display 25:00 initially in work mode',
+        (WidgetTester tester) async {
       await tester.pumpWidget(
         const MaterialApp(
-          home: TimerDial(),
+          home: TimerDial(
+            timerMode: TimerMode.work,
+          ),
         ),
       );
       expect(find.text('25:00'), findsOneWidget);
     });
 
-    testWidgets('should display 24:59 after 1 second',
+    testWidgets('should display 24:59 after 1 second in work mode',
         (WidgetTester tester) async {
       await tester.pumpWidget(
         const MaterialApp(
-          home: TimerDial(),
+          home: TimerDial(
+            timerMode: TimerMode.work,
+          ),
         ),
       );
       await tester.pump(const Duration(seconds: 1));
       expect(find.text('24:59'), findsOneWidget);
     });
 
-    testWidgets('should display 00:00 after 25 minutes',
+    testWidgets('should display 00:00 after 25 minutes in work mode',
         (WidgetTester tester) async {
       await tester.pumpWidget(
         const MaterialApp(
-          home: TimerDial(),
+          home: TimerDial(
+            timerMode: TimerMode.work,
+          ),
         ),
       );
       await tester.pump(const Duration(minutes: 25));
       expect(find.text('00:00'), findsOneWidget);
+    });
+
+    testWidgets('should display 05:00 initially in short break mode',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: TimerDial(timerMode: TimerMode.shortBreak),
+        ),
+      );
+
+      expect(find.text('05:00'), findsOneWidget);
     });
   });
 }
